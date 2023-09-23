77d0b646bddd65d3821c815e44b5e893654df910
Mahiliny <mahiliny.santhirakumar@mail.utoronto.ca>
2023-09-23 15:00:40 -0400

    Added UofT email address field

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -12,19 +12,12 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+#Add variable to error stmnt !!!!!!!
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired() , Email(message = "Please include an '@' in the email address. Email address is missing an '@' ")] )
     submit = SubmitField('Submit')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,5 +28,19 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        
+        if 'utoronto' in form.email.data:
+            valid_email = True
+        else:
+            valid_email = False
+        
+        session['email'] = form.email.data
+        session['valid_email'] = valid_email
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), valid_email = session.get('valid_email'))
